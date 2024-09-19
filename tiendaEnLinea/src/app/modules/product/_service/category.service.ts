51694d91ebcb50f92bf2634bb724c1c1c54079d7
JSRMario <juanmario@ciencias.unam.mx>
2024-09-18 21:56:55 -0600

    Update: Agrego del 5.10 hasta el final de la practica.
    * Agrego una funcion getCategories dentro de component.ts, que llama a la función getCategories de la parte 4.
    * Agrega el metodo ngOnInit que ejecuta el metodo al ingresar al componente.
    * Agrega una tabla en el archivo .html
    * Eliminamos el codigo de la p3 y agregamos la etiqueta de router-outlet
    * Se puede ejecutar usando ng serve y la tabla esta en http://localhost:4200/categoria

--- tiendaEnLinea/src/app/modules/product/_service/category.service.ts
+++ tiendaEnLinea/src/app/modules/product/_service/category.service.ts
@@ -11,9 +11,9 @@ export class CategoryService {
   getCategories(){
     // const is better than 'var' because of reach and it doesn't get reassigned
     const categories=   [
-      new Category(1, 'Electronics', 'Tag1', 'Active'), 
-      new Category(2, 'Frozen', 'Tag2', 'Inactive'),
-      new Category(3, 'Chips', 'Tag3', 'Active')
+      new Category(1, 'Electrónicos', 'Etiqueta1', 'Activo'), 
+      new Category(2, 'Congelados', 'Etiqueta2', 'Activo'),
+      new Category(3, 'Papas', 'Etiqueta3', 'Inactivo')
     ];
 
     return categories;
