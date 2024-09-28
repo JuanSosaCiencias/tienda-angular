25438cabc6dabb71e9e37063814389a9bd745e03
JSRMario <juanmario@ciencias.unam.mx>
2024-09-27 22:52:34 -0600

    update: agrego un suscriptor para ver cambios
    Queria quitar los errores una vez empezamos a escribir para que el usuario no viera errores,
    es decir, solo checa si hay errores una vez se 'guarda' y una vez escribimos borra el estado.

--- tiendaEnLinea/src/app/modules/product/component/category/category.component.ts
+++ tiendaEnLinea/src/app/modules/product/component/category/category.component.ts
@@ -13,7 +13,6 @@ declare var $: any;
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.css']  
 })
-  
 export class CategoryComponent {
   categories: any[] = [];
   swal: SwalMessages = new SwalMessages(); // swal messages
@@ -24,6 +23,15 @@ export class CategoryComponent {
     this.form = this.formBuilder.group({
       categoria: ["", [Validators.required]],
       etiqueta: ["", [Validators.required]],
+    });
+
+    // Esta parte es para resetear los errores al escribir en los campos
+    this.form.get('categoria')?.valueChanges.subscribe(() => {
+      this.submitted = false; // Resetea `submitted` al escribir
+    });
+    
+    this.form.get('etiqueta')?.valueChanges.subscribe(() => {
+      this.submitted = false; // Resetea `submitted` al escribir
     });
   }
 
@@ -45,19 +53,18 @@ export class CategoryComponent {
     $("#modalForm").modal("hide");
   }
 
-  
-
-  // función para registrar una nueva categoria
-  onSubmit(){
-    this.submitted = true; // ya se ha enviado el formulario
-    if(this.form.invalid) return; // si el formulario es inválido, se detiene la ejecución de la función
-    this.submitted = false; // se reinicia el valor de submitted
+  private addCategory(){
     let id = this.categories.length + 1; // obtenemos el id de la nueva categoria
     let region = new Category(id, this.form.controls['categoria'].value!, this.form.controls['etiqueta'].value!, 'Activo'); // creamos un objeto de tipo Category
     this.categories.push(region); // agregamos la nueva categoria al arreglo
     this.swal.successMessage("La categoria ha sido registrada"); // usamos sweetalert para dar un mensaje bonito al usuario
     this.hideModalForm(); // cerramos el modal
-    
   }
 
+  // función para registrar una nueva categoria
+  onSubmit() {
+    this.submitted = true; // ya se ha enviado el formulario
+    if (this.form.invalid) return; // si el formulario es inválido, se detiene la ejecución de la función
+    this.addCategory(); // se llama a la función para agregar una nueva categoria
+  }
 }
