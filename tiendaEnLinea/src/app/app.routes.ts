28f8479e489bea8a7e3d9c6bef8af6f41819d06e
JSRMario <juanmario@ciencias.unam.mx>
2024-10-10 23:35:29 -0600

    feat: agrega el login, register y navbar hechos con el ayudante

--- tiendaEnLinea/src/app/app.routes.ts
+++ tiendaEnLinea/src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 
 // Traemos el componente que hicimos en los pasos anteriores
 import { CategoryComponent } from './modules/product/component/category/category.component';
+import { LoginComponent } from './modules/auth/component/login/login.component';
+import { RegisterComponent } from './modules/auth/component/register/register.component';
+import { SecuredComponent } from './modules/auth/component/secured/secured.component';
+import { authenticationGuard } from './modules/auth/authentication.guard';
 
 
 export const routes: Routes = [
@@ -15,5 +19,18 @@ export const routes: Routes = [
     { 
         path: "category", 
         component: CategoryComponent 
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
+    {
+        path: 'secured',
+        component: SecuredComponent, 
+        canActivate: [authenticationGuard]
     }
 ];
